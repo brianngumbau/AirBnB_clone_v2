64191d80d420e829d0a37371a26c169da02035a2
brianngumbau <brianngumbau9075@gmail.com>
2024-01-17 01:01:43 -0800

    update

--- models/user.py
+++ models/user.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(
+            string(128), nullable=False
+            )
+    password = Column(
+            string(128), nullable=False
+            )
+    first_name = Column(
+            string(128), nullable=False
+            )
+    last_name = Column(
+            string(128), nullable=False
+            )
+    places = relationship(
+            'Place',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+            )
+    reviews = relationship(
+            'Review',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+            )
