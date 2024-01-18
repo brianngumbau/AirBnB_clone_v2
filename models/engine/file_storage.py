c3424b7a61a95f0a6511c304f7d4987e343ef25d
brianngumbau <brianngumbau9075@gmail.com>
2024-01-17 23:26:27 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,10 +35,9 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Removes an object from the storage dictionary"""
-        if obj is not None:
-            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-            if obj_key in self.__objects.keys():
-                del self.__objects[obj_key]
+        if obj:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
