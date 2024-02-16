184673cdeb30cd6d57c36cc6c9ff7b73bb889356
brianngumbau <brianngumbau9075@gmail.com>
2024-02-16 05:42:29 -0800

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    return f"C {escape(text.replace('_', ' '))}"
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
