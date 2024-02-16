e5ed0c57bd1a07b4abb100d5f77851d058599aab
brianngumbau <brianngumbau9075@gmail.com>
2024-02-16 04:51:03 -0800

    task 0

---
+++ web_flask/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+install flask
+"""
+
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
