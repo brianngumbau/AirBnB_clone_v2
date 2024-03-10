25acb74a9a4e5fe5b9bc9e059abb4f784befea5d
brianngumbau <brianngumbau9075@gmail.com>
2024-03-10 05:58:25 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,10 @@ echo '<html>
 <head>
 </head>
 <body>
-	Brian@alx
+        Brian@alx
 </body>
 </html>' > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test//data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
